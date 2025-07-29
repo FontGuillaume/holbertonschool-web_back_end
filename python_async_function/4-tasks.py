3ea832ae6c0c3086d19f00688f5dae3a0792d4d1
FontGuillaume <guillaume_font@orange.fr>
2025-07-29 14:08:06 +0200

    Amélioration de la documentation de la fonction task_wait_n pour clarifier les arguments et les retours.

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -8,17 +8,11 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Exécute n tâches asynchrones avec un délai maximum
     et retourne la liste des délais obtenus.
-
-    Args:
-        n (int): Nombre de tâches à lancer.
-        max_delay (int): Délai maximum pour chaque tâche.
-
-    Returns:
-        list: Liste des délais retournés par chaque tâche.
     """
-    # Création de la liste des tâches à exécuter
+    """ Création de la liste des tâches à exécuter """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    # Exécution concurrente de toutes les tâches et récupération des résultats
+    """ Exécution concurrente de toutes les tâches
+    et récupération des résultats """
     delay = await asyncio.gather(*tasks)
-    # Retourne la liste des délais
+    """ Retourne la liste des délais """
     return delay
