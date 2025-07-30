8496f4b15a68c9eaf850818af5e2f5ebb57e35cc
FontGuillaume <guillaume_font@orange.fr>
2025-07-30 14:50:03 +0200

    Amélioration de la docstring de la fonction async_comprehension pour clarifier son fonctionnement.

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,12 +5,11 @@ valeurs générées par async_generator dans une liste.
 """
 async_generator = __import__('0-async_generator').async_generator
 
-"""
-Fonction asynchrone qui utilise une compréhension de liste
-pour collecter les valeurs générées par async_generator.
-Retourne une liste de 10 nombres flottants.
-"""
-
 
 async def async_comprehension():
+    """
+    Utilise une compréhension de liste pour collecter
+    les valeurs générées par async_generator.
+    Retourne une liste de 10 nombres flottants.
+    """
     return [i async for i in async_generator()]
