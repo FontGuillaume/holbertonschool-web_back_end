f040e9a5a77886132da6ac3d495e474b980492c3
FontGuillaume <guillaume_font@orange.fr>
2025-07-30 14:47:53 +0200

    Réorganisation et clarification de la docstring de la fonction async_generator.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,16 +6,14 @@ seconde entre chaque génération.
 """
 import asyncio
 import random
-import typing
-
-"""
-Fonction asynchrone qui génère 10 nombres aléatoires.
-À chaque itération, elle attend 1 seconde avant de produire
-un nombre flottant compris entre 0 et 10.
-"""
 
 
 async def async_generator():
+    """
+    Fonction asynchrone qui génère 10 nombres aléatoires.
+    À chaque itération, elle attend 1 seconde avant de produire
+    un nombre flottant compris entre 0 et 10.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         result = random.uniform(0, 10)
