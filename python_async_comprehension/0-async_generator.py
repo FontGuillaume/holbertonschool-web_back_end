95d9b5548fa6a5a2365e5ef5cc8689f664d1ea36
FontGuillaume <guillaume_font@orange.fr>
2025-07-30 14:59:47 +0200

    Réorganisation de la fonction async_generator pour introduire une variable intermédiaire pour le résultat avant de le produire.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -16,5 +16,5 @@ async def async_generator():
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
-        
+        result = random.uniform(0, 10)
+        yield result
