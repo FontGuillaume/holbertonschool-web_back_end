7e1f310fbd9fb6c422c6fcaa10731f96bc66c69c
FontGuillaume <guillaume_font@orange.fr>
2025-07-30 15:19:13 +0200

    Réorganisation de la documentation de la fonction measure_runtime pour améliorer la clarté.

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,14 +7,13 @@ import time
 import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
-"""
-Coroutine qui mesure le temps d'exécution pour lancer
-quatre async_comprehension en parallèle. Retourne le
-temps total écoulé en secondes.
-"""
-
 
 async def measure_runtime() -> float:
+    """
+    Coroutine qui mesure le temps d'exécution pour lancer
+    quatre async_comprehension en parallèle. Retourne le
+    temps total écoulé en secondes.
+    """
     start_time = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     return time.time - start_time
