fb296c37ae1b8c4afc466d9339b7129cd1d3c3e4
FontGuillaume <guillaume_font@orange.fr>
2025-08-13 10:33:04 +0200

    Ajout d'une docstring pour décrire la fonction list_all dans le fichier 8-all.py

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+that function list all documents on mongo_collection
+"""
+
+
 def list_all(mongo_collection):
     """
     Liste tous les documents d'une collection MongoDB.
