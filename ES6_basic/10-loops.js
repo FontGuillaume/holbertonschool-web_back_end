af5452b0e9ede185ddff008603eb335f8779f391
FontGuillaume <guillaume_font@orange.fr>
2025-08-19 09:43:49 +0200

    Refactor appendToEachArrayValue to use for...of loop and return a new array; add createEmployeesObject function for employee management

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
+  const Result = [];
+  for (const value of array) {
+  const newValue = appendString + value;
+  Result.push(newValue);
   }
+return Result;
+  
 
-  return array;
 }
