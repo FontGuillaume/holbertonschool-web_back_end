c7df2e4997333df520b1931b4f38a9b560a3ba64
FontGuillaume <guillaume_font@orange.fr>
2025-08-20 10:17:09 +0200

    Correction de la mise en forme et ajout de nouvelles validations dans les classes Currency et Pricing. Ajout de nouvelles instances dans les fichiers principaux.

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,6 +1,7 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom.js';
+
 export default initializeRooms;
 
 function initializeRooms() {
-    return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,39 +1,39 @@
 export default class Currency {
-    constructor(code, name) {
-        if (typeof code !== "string") {
-          throw new TypeError("Code must be a string");
-        }
-        if (typeof name !== "string") {
-          throw new TypeError("Name must be a string");
-        }
-
-        this._code = code;
-        this._name = name;
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
 
-    get code() {
-        return this._code;
-    }
+    this._code = code;
+    this._name = name;
+  }
 
-    set code(value) {
-        if (typeof value !== "string") {
-          throw new TypeError("Code must be a string");
-        }
-        this._code = value;
-    }
+  get code() {
+    return this._code;
+  }
 
-    get name() {
-        return this._name;
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
     }
+    this._code = value;
+  }
 
-    set name(value) {
-        if (typeof value !== "string") {
-          throw new TypeError("Name must be a string");
-        }
-        this._name = value;
-    }
+  get name() {
+    return this._name;
+  }
 
-    displayFullCurrency() {
-        return `${this.name} (${this.code})`;
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
