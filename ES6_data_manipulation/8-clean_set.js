3c69c7a614ac4d9b6e086829a60f5690f3cfc266
FontGuillaume <guillaume_font@orange.fr>
2025-08-26 14:26:52 +0200

    Correction de la validation des paramètres dans la fonction cleanSet pour gérer les types et les valeurs d'entrée.

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,9 @@
 export default function cleanSet(set, startString) {
-    if (startString === '')
-        return '';
-    return [Array.from(set)]
-    .filter(value => value.startsWith(startString))
-    .map(value => value.slice(startString.length))
+  if (!(set instanceof Set) || typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+  return Array.from(set)
+    .filter((value) => typeof value === 'string' && value.startsWith(startString) && value !== startString)
+    .map((value) => value.slice(startString.length))
     .join('-');
-
-
 }
