4778baa09b26c05cc445139177409e1820ad6571
FontGuillaume <guillaume_font@orange.fr>
2025-09-02 09:48:26 +0200

    fix: supprimer une nouvelle ligne à la fin du message de fermeture

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,7 +3,8 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
   const name = data.trim();
   console.log(`Your name is: ${name}`);
+  process.stdin.end();
 });
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
