e4cb08c8d26b6c56b9b1601035cc6bf43c1fe15d
FontGuillaume <guillaume_font@orange.fr>
2025-09-02 14:54:49 +0200

    refactor: simplifier la création du serveur HTTP et supprimer le fichier 5-http.js

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,13 +1,10 @@
-const http = require("http");
-const host = 'localhost';
-const port = 1245;
+const http = require('http');
 
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("Hello Holberton School!");
-}
-
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
 });
+
+app.listen(1245);
+
+module.exports = app;
