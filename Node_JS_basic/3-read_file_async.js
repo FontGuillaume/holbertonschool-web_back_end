077296926e9f64038a11a31960963783157844a3
FontGuillaume <guillaume_font@orange.fr>
2025-09-02 15:37:30 +0200

    refactor: améliorer la lisibilité de la fonction countStudents en uniformisant le style de code

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -7,27 +7,27 @@ function countStudents(path) {
         reject(new Error('Cannot load the database'));
       } else {
         const lines = data.split('\n');
-        const students = lines.slice(1).filter(line => line.trim() !== '');
-        
+        const students = lines.slice(1).filter((line) => line.trim() !== '');
+
         console.log(`Number of students: ${students.length}`);
-        
+
         const fields = {};
         students.forEach((line) => {
           const parts = line.split(',');
           const firstname = parts[0].trim();
           const field = parts[3].trim();
-          
+
           if (!fields[field]) {
             fields[field] = [];
           }
           fields[field].push(firstname);
         });
-        
+
         Object.keys(fields).forEach((field) => {
           const list = fields[field].join(', ');
           console.log(`Number of students in ${field}: ${fields[field].length}. List: ${list}`);
         });
-        
+
         resolve();
       }
     });
