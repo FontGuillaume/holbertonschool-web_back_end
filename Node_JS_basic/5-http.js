d71eb03aaacad86028f51dd292df4ab52411c7a5
FontGuillaume <guillaume_font@orange.fr>
2025-09-02 16:35:32 +0200

    refactor: améliorer la gestion des requêtes dans le serveur HTTP pour une meilleure lisibilité

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,8 +1,26 @@
 const http = require('http');
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    const databasePath = process.argv[2];
+    res.write('This is the list of our students\n');
+
+    countStudents(databasePath)
+      .then(() => {
+        res.end();
+      })
+      .catch(() => {
+        res.write('Cannot load the database');
+        res.end();
+      });
+  } else {
+    res.end('Hello Holberton School!');
+  }
 });
 
 app.listen(1245);
