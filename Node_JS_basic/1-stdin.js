1cd2bf818558f2b63dc4c36ede4d0c1567acbb77
FontGuillaume <guillaume_font@orange.fr>
2025-09-03 10:15:55 +0200

    refactor: simplifier la gestion des entrées utilisateur en remplaçant l'écoute des données par un traitement direct

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.setEncoding('utf8');
-
-let hasProcessed = false;
-
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null && !hasProcessed) {
-    hasProcessed = true;
-    const name = chunk.trim();
-    console.log(`Your name is: ${name}`);
-  }
+process.stdin.on('data', (input) => {
+  process.stdout.write(`Your name is: ${input}`);
 });
-
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
