bae58d3db631d81d6f2b2200037d779aceb484d9
FontGuillaume <guillaume_font@orange.fr>
2025-09-03 09:34:16 +0200

    refactor: corriger l'écoute des données pour une meilleure gestion des entrées utilisateur

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,9 @@
-process.stdin.setEncoding('utf8');
 console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf8');
 
-process.stdin.once('data', (data) => {
+process.stdin.on('data', (data) => {
   const name = data.trim();
   console.log(`Your name is: ${name}`);
-  process.stdin.end();
 });
 
 process.stdin.on('end', () => {
