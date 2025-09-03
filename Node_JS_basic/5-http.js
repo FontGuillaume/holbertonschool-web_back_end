18ad0bc2141de9a6e1a027272d594712f3744c65
FontGuillaume <guillaume_font@orange.fr>
2025-09-03 13:52:52 +0200

    refactor: améliorer la capture des logs pour la réponse des étudiants

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -8,13 +8,33 @@ const app = http.createServer((req, res) => {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     const databasePath = process.argv[2];
+
+    // Commence la réponse
     res.write('This is the list of our students\n');
 
+    // Sauvegarder console.log
+    const originalLog = console.log;
+    let capturedOutput = '';
+
+    // Remplace console.log pour capturer
+    console.log = (message) => {
+      capturedOutput += message + '\n';
+    }
+
+    // Appel countStudents (qui va faire ses consoles.log)
     countStudents(databasePath)
       .then(() => {
+        // Restaure le console.log original
+        console.log = originalLog;
+
+        // On envoie ce qu'on a capturer au client HTTP
+        res.write(capturedOutput);
         res.end();
       })
       .catch(() => {
+        // Restaure console.log en cas d'erreur aussi 
+        console.log = originalLog;
+        
         res.write('Cannot load the database');
         res.end();
       });
