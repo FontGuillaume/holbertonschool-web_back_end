962833f75eec9ab0f99af0a8a4325a82b012d107
FontGuillaume <guillaume_font@orange.fr>
2025-09-05 16:36:01 +0200

    feat: ajouter les contrôleurs et les routes pour gérer les étudiants

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,47 @@
+import readDatabase from "../utils.js";
+
+class StudentsController {
+    static getAllStudents(request, response) {
+        const databasePath = process.argv[2];
+        readDatabase(databasePath)
+            .then((fields) => {
+                let output = 'This is the list of our students\n';
+                const sortedFields = Object.keys(fields).sort();
+
+                sortedFields.forEach((field) => {
+                    const students = fields[field];
+                    output += `Number of students in ${field}: ${students.length}. List:${students.join(', ')}\n`;
+                });
+
+                response.status(200).send(output.trim());
+            })
+
+            .catch(() => {
+                response.status(500).send('Cannot load the database');
+
+            })
+    }
+
+    static getAllStudentsByMajor(request, response) {
+        const { major } = request.params;
+        const databasePath = process.argv[2];
+        if (major !== 'CS' && major !== 'SWE') {
+            response.status(500).send('major parameter must be CS or SWE');
+            return;
+        }
+
+        readDatabase(databasePath)
+        .then((fields) => {
+            const students = fields[major] || [];
+            response.status(200).send(`List: ${students.join(', ')}`);
+
+        })
+        .catch(() => {
+            response.status(500).send('Cannot load the database');
+        });
+    }
+}
+    
+    export default StudentsController;
+    
+
