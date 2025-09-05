2b4626271ccbdcf1b5f421b78686d2de9d23aec6
FontGuillaume <guillaume_font@orange.fr>
2025-09-05 17:04:37 +0200

    fix: améliorer la lisibilité et la structure du code dans readDatabase

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,36 +1,33 @@
 import fs from 'fs';
 
 const readDatabase = (filePath) => new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            reject(new Error('Cannot load the database'));
-            return;
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    try {
+      const students = data.split('\n').slice(1);
+      const validStudents = students.filter((line) => line.trim() !== '');
+      const fields = {};
+
+      validStudents.forEach((line) => {
+        const parts = line.split(',');
+        const firstname = parts[0].trim();
+        const field = parts[3].trim();
+
+        if (!fields[field]) {
+          fields[field] = [];
         }
+        fields[field].push(firstname);
+      });
 
-        try {
-            const students = data.split('\n').slice(1);
-            const validStudents = students.filter((line) => line.trim() !== '');
-            const fields = {};
-
-            validStudents.forEach((line) => {
-                const parts = line.split(',');
-                const firstname = parts[0].trim();
-                const field = parts[3].trim();
-
-                if (!fields[field]) {
-                    fields[field] = [];
-                }
-                fields[field].push(firstname);
-            });
-                
-            resolve(fields);
-
-        } catch (error) {
-            reject(new Error('Cannot load the database'));
-        }
-    });
+      resolve(fields);
+    } catch (error) {
+      reject(new Error('Cannot load the database'));
+    }
+  });
 });
 
 export default readDatabase;
-
-        
